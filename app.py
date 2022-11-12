8aa3d19ac7398331d2b96d24a641572a5baa514b
JCBucio <juan.c4@hotmail.com>
2022-11-12 12:20:32 -0600

    Topography trace added

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
-import plotly.io as pio
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import netCDF4 as nc
 
 app = Dash(__name__)
 
 server = app.server
 # Set the default theme to 'plotly_dark'
 # pio.templates.default = 'plotly_dark'
+
+grid = nc.Dataset('data/tancitaro_sampled_150m.nc')
+x = grid['x'][:]
+y = grid['y'][:]
+z = grid['z'][:]
+z = np.round(z/1000, 3)
 
 df = pd.read_csv('data/enjambres.csv')
 df['profundidad'] = -df['profundidad']
@@ -20,14 +27,19 @@ df['fecha_hora_utc'] = df['fecha_utc'].dt.strftime('%Y-%m-%d') + 'T' + df['hora_
 markdown_text = '''
 # Enjambre sísmico Michoacán 2019 - 2022
 
-Gráfica de sismos registrados entre 2019 y 2022 en los alrededores
-de los volcanes Tancítaro y Paricutín en Michoacán, México. Datos del [SSN](http://www.ssn.unam.mx/). 
-
-Creado por [Juan Carlos Bucio T.](https://jcbucio.github.io/).
+Enjambres sísmicos registrados entre abril 2019 y agosto 2022 en los alrededores
+de los volcanes Tancítaro y Paricutín en Michoacán, México.
 '''
 datepicker_label = "#### Elije un rango de fechas UTC:"
 magnitudes_label = "#### Arrastra el slider para ver un rango diferente de magnitudes:"
+foot_label = '''
+### Referencias
 
+- Sismos del [Servicio Sismológico Nacional](http://www.ssn.unam.mx/).
+- Topografía de [OpenTopography](https://www.opentopography.org/).
+- Creado por [Juan Carlos Bucio T.](https://jcbucio.github.io/).
+- Código fuente en [GitHub](https://github.com/JCBucio/enjambre-sismico-mich).
+'''
 
 marks_range = np.arange(2, 5.5, 0.5)
 
@@ -55,7 +67,9 @@ app.layout = html.Div([
         value=[2.0, 5.0],
         marks={str(m): str(m) for m in marks_range},
         step=0.1
-    )
+    ),
+    html.Br(),
+    dcc.Markdown(foot_label)
 ], style={'fontFamily': 'monospace', 'margin': '2rem'})
 
 @app.callback(
@@ -93,9 +107,43 @@ def update_bar_chart(slider_range, start_date, end_date):
             'referencia': 'Referencia'
         }
 
-        fig = px.scatter_3d(mask, x="latitud", y="longitud", z="profundidad", size='magnitud', size_max=10,
-                            color="magnitud", hover_data=['fecha_hora_utc', 'latitud', 'longitud', 'profundidad', 'magnitud', 'referencia'],
-                            labels=tooltip_labels)
+        # fig = px.scatter_3d(mask, x="latitud", y="longitud", z="profundidad", size='magnitud', size_max=10,
+        #                     color="magnitud", hover_data=['fecha_hora_utc', 'latitud', 'longitud', 'profundidad', 'magnitud', 'referencia'],
+        #                     labels=tooltip_labels)
+
+        fig = go.Figure()
+
+        fig.add_trace(go.Surface(
+            z=z, x=x, y=y, 
+            name="Topografía", 
+            showscale=False,
+            # Set the hover template and multiply the z value by 1000 to convert from km to m
+            hovertemplate='<b>Longitud</b>: %{x}<br><b>Latitud</b>: %{y}<br><b>Elevación</b>: %{z} km',
+        ))
+
+        fig.add_trace(go.Scatter3d(
+            x=mask['longitud'],
+            y=mask['latitud'],
+            z=mask['profundidad'],
+            mode='markers',
+            marker=dict(
+                size=2,
+                color=mask['magnitud'],
+                colorscale='Viridis',
+                colorbar=dict(title='Magnitudes', titleside='bottom', xpad=0.5, ypad=0.5),
+                opacity=0.8
+            ),
+            # Set the labels in the hover tooltip
+            hovertemplate=
+                '<b>Fecha y hora UTC</b>: %{customdata[0]}<br>' +
+                '<b>Latitud</b>: %{customdata[1]}<br>' +
+                '<b>Longitud</b>: %{customdata[2]}<br>' +
+                '<b>Profundidad (km)</b>: %{customdata[3]}<br>' +
+                '<b>Magnitud</b>: %{customdata[4]}<br>' +
+                '<b>Referencia</b>: %{customdata[5]}<br>',
+            customdata=mask[['fecha_hora_utc', 'latitud', 'longitud', 'profundidad', 'magnitud', 'referencia']].values,
+            name="Sismos"            
+        ))
 
         # Format the labels
         fig.update_layout(scene = dict(
@@ -103,9 +151,6 @@ def update_bar_chart(slider_range, start_date, end_date):
                         yaxis_title='Longitud',
                         zaxis_title='Profundidad (km)'),
                         margin=dict(r=20, b=10, l=10, t=10))
-
-        # Format the labels for the colorbar
-        fig.update_layout(coloraxis_colorbar=dict(title="Magnitudes"))
 
         # Change the font family to monospace
         fig.update_layout(font_family="monospace")
